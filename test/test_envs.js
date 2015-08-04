dcbe68049dd5895f06afee26c2ee2f3ae225a5dc
Matthew Phillips <matthew@matthewphillips.info>
2015-08-04 13:48:40 -0400

    Add a System.renderingLoader for plugin use
    This facilitates the create of a System.renderingLoader Loader that is
    added after `@config` has loaded. This is where you can add a baseURL
    for an environment that should be used by plugins/extensions rather than
    the global loader's baseURL which is for the server Steal instance.

---
+++ test/test_envs.js
@@ -0,0 +1,31 @@
+var canSsr = require("../lib/");
+var helpers = require("./helpers");
+var assert = require("assert");
+var path = require("path");
+
+
+describe("rendering an app using envs", function(){
+	before(function(){
+		this.render = canSsr({
+			config: __dirname + "/tests/package.json!npm",
+			main: "envs/index.stache!done-autorender",
+			env: "someenv"
+		});
+	});
+
+	it("works", function(done){
+		this.render("/").then(function(html){
+			var node = helpers.dom(html);
+
+			var found = [];
+			helpers.traverse(node, function(el){
+				if(el.nodeName === "SPAN") {
+					found.push(el);
+				}
+			});
+
+			assert.equal(found[0].innerHTML, "hello bar", "envs config was applied");
+		}).then(done);
+	});
+
+});
