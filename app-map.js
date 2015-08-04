d58362b8b2e9a97cc9557a9677cff4a3769f1b62
Matthew Phillips <matthew@matthewphillips.info>
2015-08-04 11:22:24 -0400

    Remove dependency on can/route/pushstate
    I'm not sure why we were importing that, I think there was a mistaken
    believe that it was needed to make server-side rendering work, but since
    the application will be importing pushstate itself it is not needed
    here.

--- app-map.js
+++ app-map.js
@@ -1,4 +1,4 @@
-steal("can/util", "can/map", "can/compute", "can/route/pushstate/", function(can){
+steal("can/util", "can/map", "can/compute", function(can){
 
 	function sortedSetJson(set){
 		if(set == null) {
