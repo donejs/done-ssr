d3baa08aa117093fa1cee22ef1061e822557099c
Matthew Phillips <matthew@matthewphillips.info>
2016-04-13 08:34:22 -0400

    Set document.location on each request
    This is the same as window.location. Closes #166

--- test/tests/progressive/appstate.js
+++ test/tests/progressive/appstate.js
@@ -3,5 +3,11 @@ var Map = require("can/map/");
 module.exports = Map.extend({
 	throwError: function() {
 		throw Error('Something went wrong');
+	},
+	location: function(){
+		return location.href;
+	},
+	docLocation: function(){
+		return document.location.href;
 	}
 });
