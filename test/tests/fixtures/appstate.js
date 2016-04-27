7ac5f1be1779d1ba14750f7bd943ff8c7dd67b65
Matthew Phillips <matthew@matthewphillips.info>
2016-04-27 09:14:46 -0400

    Move XHR and WebSocket polyfills into this library
    This moves the XHR and WebSocket polyfills into this library. This is so
    that users do not need to provide their own.
    
    Closes #12

---
+++ test/tests/fixtures/appstate.js
@@ -0,0 +1,23 @@
+var List = require("can/list/");
+var Map = require("can/map/");
+require("can/map/define/");
+
+module.exports = Map.extend({
+	define: {
+		things: {
+			Value: List,
+			get: function(list){
+				var xhr = new XMLHttpRequest();
+				xhr.open("GET", "http://www.example.org/stuff");
+				xhr.addEventListener("load", function(){
+					var json = xhr.responseText;
+					var data = JSON.parse(json);
+					list.replace(data);
+				});
+				xhr.send();
+
+				return list;
+			}
+		}
+	}
+});
