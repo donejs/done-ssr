2a6e44242e82a80da91a318a3adb64419a25c22c
Marshall Thompson <marshall@creativeideal.net>
2016-08-31 12:25:39 -0600

    Allow configuring an authCookie name for token auth
    This adds an option to the xhr zone that allows you to add a cookie name where an auth token will be potentially located.  If a token is found, it copies the token to the authorization header of all outgoing requests.
    
    Also removed the token_test and set up the auth-cookie_test, since this replaces that functionality.

---
+++ test/auth-cookie_test.js
@@ -0,0 +1,53 @@
+var path = require("path");
+var assert = require("assert");
+var ssr = require("../lib/");
+var helpers = require("./helpers");
+var through = require("through2");
+
+describe("xhr async rendering", function() {
+	this.timeout(10000);
+
+	var render;
+	var xhrOptions = {};
+
+	before(function() {
+		this.oldXHR = global.XMLHttpRequest;
+		global.XMLHttpRequest = helpers.mockXHR('[1,2,3,4,5]', xhrOptions);
+
+		render = ssr({
+			config: "file:" + path.join(__dirname, "tests", "package.json!npm"),
+			main: "xhr/index.stache!done-autorender",
+			paths: {
+				"$css": "file:" + path.resolve(__dirname + "/tests/less_plugin.js")
+			}
+		}, {
+			authCookie: 'feathers-jwt'
+		});
+	});
+
+	after(function() {
+		global.XMLHttpRequest = this.oldXHR;
+	});
+
+	it("works", function(done) {
+		var didXhr = false;
+
+		xhrOptions.beforeSend = function(xhr){
+			var auth = xhr.getRequestHeader('authorization');
+			assert.equal(auth, "Bearer foobar");
+			didXhr = true;
+		};
+
+		var stream = render({
+			url: '/',
+			headers: {
+				cookie: "feathers-jwt=foobar;"
+			}
+		});
+
+		stream.pipe(through(function(buffer) {
+			assert.ok(didXhr);
+			done();
+		}));
+	});
+});
