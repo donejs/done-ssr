3ff88cb17719df2dacce45c54cb9d9d2d255f25f
Matthew Phillips <matthew@matthewphillips.info>
2017-02-15 16:40:02 -0500

    Use can.route.matched() to determine if a route matched
    This changes the behavior of 404s, so that we use the
    can.route.matched() compute to determine if a route was matched when
    rendering. This is used to determine which `statusCode` is used, if the
    user doesn't provide it themselves on their ViewModel.
    
    Also adds a few tests to make sure this works with DefineMaps. Closes

---
+++ test/tests/define-map-status/vm.js
@@ -0,0 +1,10 @@
+var DefineMap = require("can-define/map/map");
+var route = require("can-route");
+require("can-route-pushstate");
+
+route("{page}");
+
+module.exports = DefineMap.extend({
+	page: "string",
+	statusCode: "number"
+});
