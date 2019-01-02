023a2741039ad49dffd375b8d4f40b3e08000506
Matthew Phillips <matthew@matthewphillips.info>
2019-01-02 08:45:33 -0500

    Fix memory leak test
    This upgrades the done-autorender version which fixes the memory leak.

--- perf/make_leak.js
+++ perf/make_leak.js
@@ -4,34 +4,48 @@ var assert = require("assert");
 var path = require("path");
 var through = require("through2");
 
-
-global.XMLHttpRequest = helpers.mockXHR(
-	'[ { "a": "a" }, { "b": "b" } ]');
-
-var render = ssr({
-	config: "file:" + path.join(__dirname, "..", "test", "tests", "package.json!npm"),
-	main: "async/index.stache!done-autorender"
-});
-
-var renderThen = function(pth){
-	return new Promise(function(resolve, reject){
-		var stream = through(function(buffer){
-			resolve(buffer);
-		});
-		stream.on("error", reject);
-		render(pth).pipe(stream);
+async function run() {
+	let server = await helpers.createServer(8070, function(req, res){
+		var data;
+		switch(req.url) {
+			case "/bar":
+				data = [ { "a": "a" }, { "b": "b" } ];
+				res.setHeader("Content-Type", "application/json");
+				res.end(JSON.stringify(data));
+				break;
+			default:
+				throw new Error("No route for " + req.url);
+		}
 	});
-};
 
-var i = 100;
-next();
 
-function next() {
+	var render = ssr({
+		config: "file:" + path.join(__dirname, "..", "test", "tests", "package.json!npm"),
+		main: "async/index.stache!done-autorender"
+	});
 
-	if(i === 0) {
-		return;
+	var renderThen = function(pth){
+		return new Promise(function(resolve, reject){
+			var stream = through(function(buffer){
+				resolve(buffer);
+			});
+			stream.on("error", reject);
+			render(pth).pipe(stream);
+		});
+	};
+
+	var i = 100;
+	next();
+
+	function next() {
+		if(i === 0) {
+			server.close();
+			return;
+		}
+
+		i--;
+		renderThen("/").then(next);
 	}
-
-	i--;
-	renderThen("/").then(next);
 }
+
+run();
