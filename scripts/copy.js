1891d024aaa0b21d7d72b9b6a85a310417d63dea
Bradley Momberger <brad@bitovi.com>
2019-12-17 02:17:14 -0500

    Update deps and remove nodelists reference from zones

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,3 +1,5 @@
 var copy = require("copy-dir").sync;
 
-copy("node_modules", "test/tests/node_modules");
+copy("node_modules", "test/tests/node_modules", function(state, filePath, fileName) {
+    return fileName !== '_mocha' && filePath.indexOf(".git/") < 0;
+});
